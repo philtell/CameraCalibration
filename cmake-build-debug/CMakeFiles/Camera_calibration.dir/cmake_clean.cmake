d97be6ae24aa8729a80b576cb756c5ad7f6f507c
philtell <2267507789@qq.com>
2020-06-21 00:13:30 +0800

    second commit

---
+++ cmake-build-debug/CMakeFiles/Camera_calibration.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Camera_calibration.dir/main.cpp.o"
+  "Camera_calibration.pdb"
+  "Camera_calibration"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Camera_calibration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
